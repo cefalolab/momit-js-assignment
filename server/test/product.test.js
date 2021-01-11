fcfa164b01bd4525162991cfe1dc942ab78029a9
Momit Rahman <momitrahman@gmail.com>
2021-01-11 16:27:28 +0600

    Test: Product /products and /products/:id API

---
+++ server/test/product.test.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const server = require('../../server.js');
+
+chai.should();
+chai.use(chaiHttp);
+
+// Main block
+describe('Product:', () => {
+  // get all products
+  describe('GET /products', () => {
+    it('Should get all products', done => {
+      chai
+        .request(server)
+        .get('/api/products')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+
+  // fetch single products
+  describe('GET /products/:id', () => {
+    it('Should get all products', done => {
+      chai
+        .request(server)
+        .get('/api/products/100')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+});
